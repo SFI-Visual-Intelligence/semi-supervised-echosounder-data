7cf4c75118b7195d97d28f95c0f34436ec5efb6c
changkyuchoi <changkyu.choi@uit.no>
2020-06-30 13:36:29 +0200

    Update db_with_limits.py

--- batch/data_transform_functions/db_with_limits.py
+++ batch/data_transform_functions/db_with_limits.py
@@ -12,6 +12,4 @@ def db_with_limits_img(data):
     data = db(data)
     data[data>0] = 0
     data[data<-75] = -75
-    print(np.shape(data))
-    # data = (data - data.min()) / (data.max() - data.min())
     return data
