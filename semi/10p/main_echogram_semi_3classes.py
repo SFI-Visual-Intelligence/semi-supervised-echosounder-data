f2ba18aafa3dc703256aa12b2cff2375c43ba0a6
changkyuchoi <changkyu.choi@uit.no>
2021-07-29 22:54:11 +0200

    Update main_echogram_semi_3classes.py

--- semi/10p/main_echogram_semi_3classes.py
+++ semi/10p/main_echogram_semi_3classes.py
@@ -89,13 +89,13 @@ def parse_args():
                         reassignments of clusters (default: 1)""")
     parser.add_argument('--workers', default=4, type=int,
                         help='number of data loading workers (default: 4)')
-    parser.add_argument('--epochs', type=int, default=1,
+    parser.add_argument('--epochs', type=int, default=500,
                         help='number of total epochs to run (default: 200)')
     parser.add_argument('--pretrain_epoch', type=int, default=0,
                         help='number of pretrain epochs to run (default: 200)')
     parser.add_argument('--start_epoch', default=0, type=int,
                         help='manual epoch number (useful on restarts) (default: 0)')
-    parser.add_argument('--save_epoch', default=100, type=int,
+    parser.add_argument('--save_epoch', default=50, type=int,
                         help='save features every epoch number (default: 20)')
     parser.add_argument('--batch', default=1, type=int,
                         help='mini-batch size (default: 16)')
@@ -103,7 +103,7 @@ def parse_args():
                         help='pca dimension (default: 128)')
     parser.add_argument('--checkpoints', type=int, default=50,
                         help='how many iterations between two checkpoints (default: 25000)')
-    parser.add_argument('--display_count', type=int, default=200,
+    parser.add_argument('--display_count', type=int, default=40,
                         help='display iterations for every <display_count> numbers')
     parser.add_argument('--seed', type=int, default=31, help='random seed (default: 31)')
     parser.add_argument('--verbose', type=bool, default=True, help='chatty')
